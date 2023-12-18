73a6fb3955a1e03c1c6a2a88f5e78182d3d2f88e
starsone <starsone@foxmail.com>
2023-12-18 10:48:52 +0800

    更新remixicon为4.0.0版本
    (cherry picked from commit 2b8df01c1b0beee46adbf804dbb85095db63edd6)

--- src/main/kotlin/com/example/common/util/RemixIconDataUtil.kt
+++ src/main/kotlin/com/example/common/util/RemixIconDataUtil.kt
@@ -9,21 +9,26 @@ object RemixIconDataUtil {
     val resourceDir = System.getProperty("compose.application.resources.dir")
 
     const val dirPath = "remixicon-data"
+
+    const val jsonName = "remixicon_v4.0.0.json"
+
     fun initResource(): List<IconGroup> {
 
 
         val pair = if (resourceDir == null) {
+            //如果是直接运行MainKt就会进入此逻辑
             //例: D:\java-project\IconDrawableGenerator
             val projectRootDir = File("").absolutePath
 
-            val tagJson = File(projectRootDir,"resources\\common\\remixicon_tags.json").readText()
+            val tagJson = File(projectRootDir,"resources/common/remixicon_tags.json").readText()
             val remixIconJson =
-                File(projectRootDir,"\\resources\\common\\remixicon_v3.7.0.json").readText()
+                File(projectRootDir,"/resources/common/${jsonName}").readText()
             Pair(tagJson, remixIconJson)
 
         } else {
+            //打包成对应系统的二进制文件(exe等)会进入此逻辑
             val tagJson = File(resourceDir).resolve("remixicon_tags.json").readText()
-            val remixIconJson = File(resourceDir).resolve("remixicon_v3.7.0.json").readText()
+            val remixIconJson = File(resourceDir).resolve(jsonName).readText()
             Pair(tagJson, remixIconJson)
         }
 
@@ -73,7 +78,10 @@ object RemixIconDataUtil {
 
     fun getSvgDirFile(): File {
         if (resourceDir == null) {
-            return File("D:\\java-project\\IconDrawableGenerator\\build\\compose\\binaries\\main\\app\\IconDrawableGenerator\\app\\resources\\remixicon-data")
+            //当前项目路径,如D:\java-project\IconDrawableGenerator
+            val projectRootDir = File("").absolutePath
+            
+            return File("${projectRootDir}/build/compose/binaries/main/app/IconDrawableGenerator/app/resources/remixicon-data")
         }
         return File(resourceDir, dirPath)
     }
