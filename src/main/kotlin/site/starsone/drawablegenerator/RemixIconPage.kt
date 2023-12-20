baeaaf24dbdf3d1790ada7b2dc6420afeccaca1c
starsone <starsone@foxmail.com>
2023-12-20 19:14:25 +0800

    增加选中图标的对话框选择操作

--- src/main/kotlin/site/starsone/drawablegenerator/RemixIconPage.kt
+++ src/main/kotlin/site/starsone/drawablegenerator/RemixIconPage.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -119,22 +121,91 @@ fun RemixIconItemView(icon: Icon) {
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun RemixIconSingleView(svgFile: File) {
     var isDialogOpen by remember { mutableStateOf(false) }
+    var xmlSizeText by remember { mutableStateOf("24") }
+    var pngSizeText by remember { mutableStateOf("200") }
 
     if (isDialogOpen) {
         AlertDialog(
-            onDismissRequest = { },
-            confirmButton = {
-                Button(onClick = { isDialogOpen = false }) {
-                    Text("OK")
+            //点击外层空白处关闭
+            onDismissRequest = { isDialogOpen = false }
+        ) {
+            Column(modifier = Modifier.background(Color.White, RoundedCornerShape(24.dp)).padding(24.dp)) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Box(Modifier.border(1.dp, Color.Black).padding(8.dp)) {
+                        Image(
+                            loadSvgPainter(svgFile.inputStream(), Density(24f)),
+                            contentDescription = null,
+                            modifier = Modifier.width(24.dp).height(24.dp)
+                        )
+                    }
+
+                    Text(svgFile.nameWithoutExtension)
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Default.Favorite,
+                            contentDescription = "Favorite Icon",
+                            tint = Color.Red,
+                            modifier = Modifier.size(24.dp)
+                        )
+                    }
                 }
-            },
-            title = { Text("Alert Dialog") },
-            text = { Text("Lore ipsum") },
-        )
+
+                Spacer(Modifier.height(8.dp))
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    OutlinedTextField(
+                        xmlSizeText,
+                        onValueChange = { xmlSizeText = it },
+                        label = { Text("输出XML文件的宽高(单位dp)") })
+                    Spacer(Modifier.width(16.dp))
+                    Button(onClick = {
+                        isDialogOpen = false
+
+                        val outputFile = CommonUtil.svgToXml(svgFile, xmlSizeText.toInt())
+                        CommonUtil.copyFile(outputFile)
+
+                        ComposeToast.show("导出xml文件成功,已复制文件到剪切板,可以直接粘贴!")
+
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.KeyboardArrowDown,
+                            contentDescription = "Favorite Icon",
+                            tint = Color.Red,
+                            modifier = Modifier.size(24.dp)
+                        )
+                        Text("XML")
+                    }
+                }
+
+                Spacer(Modifier.height(8.dp))
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    OutlinedTextField(
+                        pngSizeText,
+                        onValueChange = { pngSizeText = it },
+                        label = { Text("输出PNG文件的宽高(单位px)") })
+                    Spacer(Modifier.width(16.dp))
+                    Button(onClick = {
+                        val outputFile = CommonUtil.svgToPng(svgFile, Color.White, pngSizeText.toInt())
+                        CommonUtil.copyFile(outputFile)
+                        ComposeToast.show("导出png文件成功,已复制文件到剪切板,可以直接粘贴!")
+                        isDialogOpen = false
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.KeyboardArrowDown,
+                            contentDescription = "Favorite Icon",
+                            tint = Color.Red,
+                            modifier = Modifier.size(24.dp)
+                        )
+                        Text("PNG")
+                    }
+                }
+            }
+        }
     }
 
     var active by remember { mutableStateOf(false) }
@@ -142,25 +213,13 @@ fun RemixIconSingleView(svgFile: File) {
     Column(
         modifier = Modifier
             .size(120.dp, 100.dp)
-            .background(color =  if (active) MaterialTheme.colorScheme.background else Color.White)
+            .background(color = if (active) MaterialTheme.colorScheme.background else Color.White)
             .border(width = 1.dp, color = if (active) MaterialTheme.colorScheme.outline else Color.White)
             .onPointerEvent(PointerEventType.Enter) { active = true }
             .onPointerEvent(PointerEventType.Exit) { active = false }
-            .onClick {
-
-                val outputFile = CommonUtil.svgToXml(svgFile)
-                CommonUtil.copyFile(outputFile)
-
-                ComposeToast.show("导出xml文件成功,可以直接粘贴!")
-
-                //todo 转为png或xml
-
-                //todo 输出文件png自定义
-                //CommonUtil.svgToPng(file, File("D:/jkkk.png"))
-
-                //isDialogOpen = true
-            }
-        ,
+            .onClick { //显示对话框
+                isDialogOpen = true
+            },
 
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
