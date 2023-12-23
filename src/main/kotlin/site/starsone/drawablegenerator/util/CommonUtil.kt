d641152597f4d0b2657a6687b9f48df599efe140
starsone <starsone@foxmail.com>
2023-12-23 17:09:09 +0800

    新增复制多个文件方法
    (cherry picked from commit 6fb5df7fdb6883dce03c446c4fd38e3bf1ca1f23)

--- src/main/kotlin/site/starsone/drawablegenerator/util/CommonUtil.kt
+++ src/main/kotlin/site/starsone/drawablegenerator/util/CommonUtil.kt
@@ -3,21 +3,14 @@ package site.starsone.drawablegenerator.util
 import androidx.compose.ui.graphics.toArgb
 import com.google.gson.Gson
 import com.google.gson.internal.`$Gson$Types`
-import org.apache.batik.anim.dom.SAXSVGDocumentFactory
-import org.apache.batik.svggen.SVGGraphics2D
-import org.jetbrains.skia.svg.SVGDOM
 import site.starsone.svg2vector.SvgUtils
 import java.awt.Color
-import java.awt.SystemColor.text
 import java.awt.Toolkit
 import java.awt.datatransfer.DataFlavor
 import java.awt.datatransfer.StringSelection
 import java.awt.datatransfer.Transferable
 import java.awt.datatransfer.UnsupportedFlavorException
-import java.awt.image.BufferedImage
 import java.io.File
-import java.io.IOException
-import javax.imageio.ImageIO
 
 object CommonUtil {
 
@@ -27,11 +20,26 @@ object CommonUtil {
         systemClipboard.setContents(selection, null)
     }
 
+    /**
+     * 复制单个文件
+     */
     fun copyFile(file: File) {
         // 创建剪切板内容
         val systemClipboard = Toolkit.getDefaultToolkit().systemClipboard
-        systemClipboard.setContents(FileTransferable(file), null)
+        systemClipboard.setContents(FileTransferable(listOf(file)), null)
     }
+
+
+    /**
+     * 复制多个文件
+     */
+    fun copyFile(files: List<File>) {
+        // 创建剪切板内容
+        val systemClipboard = Toolkit.getDefaultToolkit().systemClipboard
+        systemClipboard.setContents(FileTransferable(files), null)
+    }
+
+
 
     /**
      * 将json数据转为List<T>
@@ -84,26 +92,23 @@ object CommonUtil {
     }
 }
 
-private class FileTransferable(private val file: File) : Transferable {
+
+class FileTransferable(private val fileList: List<File>) : Transferable {
     override fun getTransferDataFlavors(): Array<DataFlavor> {
-        return arrayOf(FILE_FLAVOR)
+        return arrayOf(DataFlavor.javaFileListFlavor)
     }
 
     override fun isDataFlavorSupported(flavor: DataFlavor): Boolean {
-        return flavor.equals(FILE_FLAVOR)
+        return flavor.equals(DataFlavor.javaFileListFlavor)
     }
 
-    @Throws(UnsupportedFlavorException::class, IOException::class)
+    @Throws(UnsupportedFlavorException::class)
     override fun getTransferData(flavor: DataFlavor): Any {
-        return if (flavor.equals(FILE_FLAVOR)) {
-            ArrayList(listOf(file))
+        return if (flavor.equals(DataFlavor.javaFileListFlavor)) {
+            fileList
         } else {
             throw UnsupportedFlavorException(flavor)
         }
-    }
-
-    companion object {
-        private val FILE_FLAVOR = DataFlavor.javaFileListFlavor
     }
 }
 
